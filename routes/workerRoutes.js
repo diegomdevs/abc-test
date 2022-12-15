3149a92785d60dde9e7f064a543d5e349a965913
Diego Gonzalez <diegom.develops@hotmail.com>
2022-12-14 17:56:17 -0700

    feat: routes
    Adding CRUD logic and correct db connection

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -1,42 +1,55 @@
 import Worker from "../models/workerModel.js";
 
 const api = {
-  url: "/api/v1/workers",
+  url: "/api/v1/",
+  workersRoute: `/api/v1/workers/`,
   get: "GET",
+  post: "POST",
+  put: "PUT",
+  delete: "DELETE",
 };
 
+const expRegDeLaUrlConId = /\/api\/v1\/workers\/([0-9]+)\//;
+
+async function verificarUrlWithId(urlToVerify) {
+  return urlToVerify.match(/\/api\/v1\/workers\/([0-9]+)/);
+}
+
+function extraerIdDeLaUrl(url, idPosition) {
+  return url.split("/")[idPosition];
+}
+
 const router = async function (req, res) {
-  if (req.url === api.url && req.method === api.get) {
+  // GET Method
+  if (req.url === "/api/v1/workers" && req.method === api.get) {
     const workers = await Worker.find();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(workers));
   }
-  if (
-    req.url.match(new RegExp(`${api.url}/([0-9]+)`)) &&
-    req.method === "GET"
-  ) {
+
+  // GET Method with an id
+  if (req.url.match(/\/api\/v1\/workers\/([0-9]+)/) && req.method === api.get) {
     try {
-      const id = req.url.split("/")[3];
+      const id = extraerIdDeLaUrl(req.url, 4);
       const worker = await Worker.findById(id);
-      if (worker) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(worker));
-      } else {
-        throw new Error("El trabajador no esta registrado");
+      if (!worker) {
+        throw new Error("El empleado no esta registrado");
       }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(worker));
     } catch (error) {
       res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: error }));
+      res.end(JSON.stringify({ message: error.message }));
     }
   }
-  if (req.url === api.url && req.method === "POST") {
+
+  // POST method
+  if (req.url === "/api/v1/workers" && req.method === "POST") {
     try {
       let body = "";
-
-      req.on("data", () => {
+      req.on("data", (chunk) => {
         body += chunk.toString();
       });
-
       req.on("end", async () => {
         let worker = new Worker(JSON.parse(body));
         await worker.save();
@@ -46,6 +59,52 @@ const router = async function (req, res) {
     } catch (error) {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: error }));
+    }
+  }
+  // Metodo PUT con un id
+  if (req.url.match(/\/api\/v1\/workers\/([0-9]+)/) && req.method === api.put) {
+    try {
+      const id = extraerIdDeLaUrl(req.url, 4);
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", async () => {
+        let updatedWorker = await Worker.findByIdAndUpdate(
+          id,
+          JSON.parse(body),
+          {
+            new: true,
+          }
+        );
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(updatedWorker));
+      });
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error.message }));
+    }
+  }
+
+  // Metodo DELETE usando un id
+  if (
+    req.url.match(/\/api\/v1\/workers\/([0-9]+)/) &&
+    req.method === api.delete
+  ) {
+    try {
+      const id = extraerIdDeLaUrl(req.url, 4);
+      const deletedWorker = await Worker.findByIdAndDelete(id);
+
+      if (!deletedWorker) {
+        throw new Error("El empleado no esta registrado");
+      }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({ message: "El empleado fue eliminado correctamente" })
+      );
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error.message }));
     }
   }
 };
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 // Importamos el modulo moongose el cual nos ayuda a establecer una
 // conexion con una base de datos MongoDB.
+import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
-
-// Con el metodo "connect" inicializamos la conexion a la base de datos
-// de este servidor pasandole la direccion de la base de datos.
-mongoose.connect(
-  "mongodb+srv://root:casa@cluster0.owop3xo.mongodb.net/?retryWrites=true&w=majority"
-);
+import router from "./routes/workerRoutes.js";
 
 // Importamos el modulo HTTP que nos permite usar caracteristicas
 // del protocolo HTTP y nos permitira configurar nuestro servidor.
 import http from "http";
 
+const uri =
+  "mongodb+srv://root:root@cluster0.owop3xo.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri);
+
 // Con el metodo "createServer" se inicializa el servidor y le
 // aplicamos algunas configuraciones.
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  router(req, res);
+});
+
+const PORT = process.env.PORT || 5000;
 
 // Con el metodo "listen" de nuestro servidor ya inicializado le asignamos
 // un puerto para que escuche y responda las peticiones.
-server.listen(3000, console.log("Hello, World!"));
+server.listen(PORT, () => {
+  console.log(`El servidor esta corriendo en el puerto: ${PORT}`);
+});
